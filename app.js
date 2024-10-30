daf68c6f0fd401cfa4d5ee7f87545efecbef45f2
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-10-31 04:13:22 +0530

    update

--- app.js
+++ app.js
@@ -2,9 +2,26 @@ const express = require('express')
 const socket = require('socket.io')
 const http = require('http')
 const {Chess} = require('chess.js')
-
+const path = require('path')
 
 const app = express()
+
 const server = http.createServer(app);
-socket(server)
+const io = socket(server)
+
+const chess = new Chess();
+let players = {};
+let currentPlayer = "W";
+
+
+app.set("view engine", "ejs")
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get('/', (req,res)=>{
+    res.render("index")
+})
+
+server.listen(3000, function(){
+    console.log('listening on: 3000')
+})
 
