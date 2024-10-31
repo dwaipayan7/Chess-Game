10e7ec1c4104b587b5f040377725d80b8fd00fd5
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-11-01 04:50:13 +0530

    setup the chessgame.js

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,2 +1,16 @@
-const socket = io()
+const { Chess } = require("chess.js")
+
 // socket.emit("Dwaipayan")
+const socket = io()
+const chess = new Chess()
+const boardElement = document.querySelector(".chessboard")
+
+let draggedPiece = null;
+let draggedSquare = null;
+let playerRole = null;
+
+const renderBoard = ()=>{}
+
+const handleMove = () => {}
+
+const getPieceUnicode = () =>{}
